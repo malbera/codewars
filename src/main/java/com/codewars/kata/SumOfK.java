446d6d8d44a6fbd2d6c8a35617024cfbac950a30
Maxim Ralovec <m.ralovec@gmail.com>
2019-09-07 18:01:31 +0200

    Best travel

---
+++ src/main/java/com/codewars/kata/SumOfK.java
@@ -0,0 +1,10 @@
+package com.codewars.kata;
+
+import java.util.List;
+
+public class SumOfK {
+
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        return 1;
+    }
+}
---
+++ src/test/java/com/codewars/kata/SumOfKTest.java
@@ -0,0 +1,26 @@
+package com.codewars.kata;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumOfKTest {
+
+    @Test
+    void chooseBestSum() {
+        System.out.println("****** Basic Tests small numbers******");
+        List<Integer> ts = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
+        int n = SumOfK.chooseBestSum(163, 3, ts);
+        assertEquals(163, n);
+        ts = new ArrayList<>(Arrays.asList(50));
+        Integer m = SumOfK.chooseBestSum(163, 3, ts);
+        assertEquals(null, m);
+        ts = new ArrayList<>(Arrays.asList(91, 74, 73, 85, 73, 81, 87));
+        n = SumOfK.chooseBestSum(230, 3, ts);
+        assertEquals(228, n);
+    }
+}
