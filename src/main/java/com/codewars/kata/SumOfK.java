781f92209a6c289cb73d84749482c44767972d04
malbera <m.ralovec@gmail.com>
2019-09-08 20:59:29 +0200

    Best travel

--- src/main/java/com/codewars/kata/SumOfK.java
+++ src/main/java/com/codewars/kata/SumOfK.java
@@ -4,7 +4,73 @@ import java.util.List;
 
 public class SumOfK {
 
-    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
-        return 1;
+    static class CityMeta {
+        final int maxAllowedDistance;
+        final int [] citiesByDistance;
+
+        CityMeta(int maxAllowedDistance, int[] citiesByDistance) {
+            this.maxAllowedDistance = maxAllowedDistance;
+            this.citiesByDistance = citiesByDistance;
+        }
     }
+
+    static class BestCityTrip {
+        final CityMeta meta;
+        final int citiesToVisit;
+        final int cityDistance;
+        final int currentPosition;
+        final BestCityTrip nextCity;
+
+        BestCityTrip(CityMeta meta,
+                     int citiesToVisit,
+                     int cityDistance,
+                     int currentPosition,
+                     BestCityTrip nextCity) {
+            this.meta = meta;
+            this.citiesToVisit = citiesToVisit;
+            this.cityDistance = cityDistance;
+            this.currentPosition = currentPosition;
+            this.nextCity = nextCity;
+        }
+
+        BestCityTrip addCity(int cityDistance, int currentPosition) {
+            return new BestCityTrip(meta, citiesToVisit - 1, cityDistance, currentPosition + 1, this);
+        }
+
+        int calculateDistance() {
+            return nextCity == null ? cityDistance : cityDistance + nextCity.calculateDistance();
+        }
+    }
+
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        int[] cityDistance = ls.stream().mapToInt(i -> i).toArray();
+
+        CityMeta meta = new CityMeta(t, cityDistance);
+        BestCityTrip cityTrip = new BestCityTrip(meta, k, 0, 0, null);
+
+        int result = calculateCityDistance(cityTrip);
+        return result == -1 ? null : result;
+    }
+
+    static int calculateCityDistance(BestCityTrip trip) {
+        int result = -1;
+        for (int i = trip.currentPosition; i <= trip.meta.citiesByDistance.length - trip.citiesToVisit; i++) {
+            if (trip.citiesToVisit == 1) {
+                int tmp = trip.calculateDistance() + trip.meta.citiesByDistance[i];
+                if (tmp == trip.meta.maxAllowedDistance) {
+                    return tmp;
+                }
+                if (tmp > result && tmp <= trip.meta.maxAllowedDistance) {
+                    result = tmp;
+                }
+                continue;
+            }
+            int tmp = calculateCityDistance(trip.addCity(trip.meta.citiesByDistance[i], i));
+            if (tmp > result) {
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
 }
--- src/test/java/com/codewars/kata/SumOfKTest.java
+++ src/test/java/com/codewars/kata/SumOfKTest.java
@@ -11,8 +11,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class SumOfKTest {
 
     @Test
-    void chooseBestSum() {
-        System.out.println("****** Basic Tests small numbers******");
+    void BasicTests2() {
+        List<Integer> ts = new ArrayList<>(Arrays.asList(100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34));
+        int n = SumOfK.chooseBestSum(230, 4, ts);
+        assertEquals(230, n);
+        ts = new ArrayList<>(Arrays.asList(100, 76, 56, 44, 89, 73, 68, 56, 64, 123, 2333, 144, 50, 132, 123, 34));
+        n = SumOfK.chooseBestSum(100, 2, ts);
+        assertEquals(100, n);
+    }
+
+    @Test
+    void BasicTests1() {
         List<Integer> ts = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
         int n = SumOfK.chooseBestSum(163, 3, ts);
         assertEquals(163, n);
