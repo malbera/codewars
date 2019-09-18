14c3f666006c36d1f3aad7335a792e624ff6993a
malbera <m.ralovec@gmail.com>
2019-09-18 06:59:39 +0200

    MorseCode decoder advanced

--- src/main/java/com/codewars/kata/MorseCodeDecoder.java
+++ src/main/java/com/codewars/kata/MorseCodeDecoder.java
@@ -6,7 +6,37 @@ import java.util.stream.Collectors;
 
 public class MorseCodeDecoder {
 
-    public static String decode(String morseCode) {
+    private static final String WORD_DELIMITER = "00000000000000";
+    private static final String LETTER_DELIMITER = "000000";
+    private static final String DASH = "111111";
+    private static final String DOT = "11";
+    private static final String SPACE = "00";
+    private static final String SPACE_C = "00";
+    private static final String DOT_C = "01";
+
+    public static String decodeBits(String bits) {
+        StringBuilder sb = new StringBuilder();
+        String wordDelimiter = findDelimiterPatter(bits, WORD_DELIMITER);
+        String word = findDelimitedPayloadFirstElement(bits, wordDelimiter);
+        String letterDelimiter = findDelimiterPatter(word, LETTER_DELIMITER);
+        String letter = findDelimitedPayloadFirstElement(word, letterDelimiter);
+//        String dash = findDashPattern()
+
+        return null;
+    }
+
+    private static String findDelimitedPayloadFirstElement(String payload,
+                                               String delimiter) {
+        return delimiter == null ? payload : payload.split(delimiter)[0];
+    }
+
+    private static String findDelimiterPatter(String payload,
+                                              String delimiter) {
+        if (delimiter.length() == 1) return null;
+        return payload.contains(delimiter) ? delimiter : findDelimiterPatter(payload, delimiter.substring(0, delimiter.length() - 1));
+    }
+
+    public static String decodeMorse(String morseCode) {
         return Arrays.stream(morseCode.trim().split("   "))
                 .map(MorseCodeDecoder::parseWord)
                 .collect(Collectors.joining(" "));
